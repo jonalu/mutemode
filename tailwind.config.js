0d4d32fc0915c8a49d678f80a3827707af4b5c60
Jonas Lundeland <jonas.lundeland@tv2.no>
2022-08-25 20:40:12 +0200

    chore: add google fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
         "./src/**/*.{html,js,ts,jsx,tsx}",
     ],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                'monton': ['"Monoton"', 'cursive'],
+                'coda': ['"Coda"', 'cursive'],
+            },
+        },
     },
     plugins: [],
 }
